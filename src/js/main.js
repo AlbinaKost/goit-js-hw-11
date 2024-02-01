b37210b2f288852b9ea99c970a216b4eef6f5dd9
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-02-01 19:30:52 +0200

    update file

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
